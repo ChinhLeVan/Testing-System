fa0d156fb287160c58aaf8812d004116bc945b00
ChinhLeVan <48466073+ChinhLeVan@users.noreply.github.com>
2021-08-07 15:08:02 +0700

    commit
    commit

--- Buoi4/ExtraAssignment_4/Insert_Data_Extra4.sql
+++ Buoi4/ExtraAssignment_4/Insert_Data_Extra4.sql
@@ -1,7 +1,7 @@
 USE management_employee;
 
 INSERT INTO department(department_name)
-	VALUES 				('Senior developer'),
+VALUES 					('Senior developer'),
 						('Technical leader'),
 						('software'),
 						('CTO'),
@@ -14,7 +14,7 @@ INSERT INTO department(department_name)
                                 
 							
 INSERT INTO employee_table(employee_name, department_number)
-	VALUES 					('TRAN THAI HOANG',		1),
+VALUES 						('TRAN THAI HOANG',		1),
 							('NGUYEN VAN THANG', 	2),
 							('PHAM THI NGA',		4),
 							('VU MINH TUAN', 		3),
@@ -26,13 +26,13 @@ INSERT INTO employee_table(employee_name, department_number)
 							('NGUYEN TUYET MAI',	10);		
 
 INSERT INTO Employee_skill_table(Employee_number, Skill_code, Date_registered)
-VALUES 							(1, 	'JAVA',		'2020/02/07'),
-								(1, 	'ML', 		'2020/03/08'),
-								(2, 	'AI',		'2020/03/08'),
-								(1, 	'JS', 		'2020/02/08'),
-								(3, 	'NODEJS',	'2020/04/04'),
-								(4, 	'C#',		'2020/03/10'),
-								(2, 	'C++',		'2020/05/07'),
-								(5, 	'HTML',		'2020/01/02'),
-								(6, 	'PHP',		'2020/02/09'),
-								(7, 	'RUBY',		'2020/04/06');		
+VALUES 										(1, 	'JAVA',		'2020/02/07'),
+											(1, 	'ML', 		'2020/03/08'),
+											(2, 	'AI',		'2020/03/08'),
+											(1, 	'JS', 		'2020/02/08'),
+											(3, 	'NODEJS',	'2020/04/04'),
+											(4, 	'C#',		'2020/03/10'),
+											(2, 	'C++',		'2020/05/07'),
+											(5, 	'HTML',		'2020/01/02'),
+											(6, 	'PHP',		'2020/02/09'),
+											(7, 	'RUBY',		'2020/04/06');		
--- Buoi5/ExtraAssignment5.sql
+++ Buoi5/ExtraAssignment5.sql
@@ -30,3 +30,34 @@ FROM product
 WHERE ListPrice = (SELECT min_list_price
 					FROM list_price);
 
+-- ========================= Exercise 2 JOIN nhiều bảng ===============================
+-- ================================ Question 1 =====================================
+SELECT c.`Name` AS Country, s.`Name` AS Province -- COUNT(s.`Name`)
+FROM countryregion c
+JOIN stateprovince s ON c.CountryRegionCode = s.CountryRegionCode;
+
+-- ================================ Question 2 =====================================
+SELECT c.`Name` AS Country, s.`Name` AS Province -- , COUNT(s.`Name`)
+FROM countryregion c
+JOIN stateprovince s ON c.CountryRegionCode = s.CountryRegionCode
+WHERE c.`Name` = "Canada" OR c.`Name` = "Germany"
+ORDER BY Country ASC;
+
+-- ================================ Question 3 =====================================
+SELECT soh.SalesOrderID, soh.OrderDate, soh.SalesPersonID, sp.Bonus, sp.SalesYTD -- , COUNT(soh.OnlineOrderFlag)
+FROM salesorderheader soh
+JOIN salesperson sp ON soh.SalesPersonID = sp.SalesPersonID
+WHERE soh.OnlineOrderFlag = 0 AND soh.SalesPersonID IS NOT NULL;
+
+
+-- ================================ Question 4 =====================================
+CREATE OR REPLACE VIEW table_info_orders AS(
+SELECT soh.SalesOrderID, soh.OrderDate, soh.SalesPersonID, sp.Bonus, sp.SalesYTD -- , COUNT(soh.OnlineOrderFlag)
+FROM salesorderheader soh
+JOIN salesperson sp ON soh.SalesPersonID = sp.SalesPersonID
+WHERE soh.OnlineOrderFlag = 0 AND soh.SalesPersonID IS NOT NULL);
+
+SELECT tio.SalesOrderID, tio.OrderDate, e.Title AS Jobtitle, tio.Bonus, tio.SalesYTD -- , COUNT(tio.SalesYTD)
+FROM table_info_orders tio
+JOIN employee e ON tio.SalesPersonID = e.EmployeeID;
+
